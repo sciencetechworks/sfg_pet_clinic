dc13e7148fa5970be3274f97071c463974a5a1b0
ScienceTechWorks <sciencetechworks@gmail.com>
2021-11-12 15:09:39 +0100

    Update Map Impl to manage setting of ID Property. Closes #17

--- src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -4,18 +4,21 @@
  */
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
  *
  * @author Usuario
  */
-public abstract class AbstractMapService<T,ID> {
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
     
-    protected Map<ID,T> map = new HashMap<>();
+    protected Map<Long,T> map = new HashMap<>();
     
    Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -25,8 +28,15 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
     
-    T save(ID id,T object){
-        map.put(id,object);
+    T save(T object){
+        if (object!=null){
+            if (object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
     
@@ -36,6 +46,16 @@ public abstract class AbstractMapService<T,ID> {
     
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+    
+    private Long getNextId(){
+       Long nextId = null;
+       try {
+        nextId=Collections.max(map.keySet())+1;
+       } catch (NoSuchElementException e){
+           nextId = 1L;
+       }
+       return nextId;
     }
     
 }
