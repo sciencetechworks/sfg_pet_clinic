04522745d64962f431f05b1b39acd300ec5b32f2
ScienceTechWorks <sciencetechworks@gmail.com>
2021-11-15 16:33:09 +0100

    Create Pet Type, Pet, and Visit Entities. Closes #20

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Visit extends BaseEntity{
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    /**
+     * @return the date
+     */
+    public LocalDate getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the pet
+     */
+    public Pet getPet() {
+        return pet;
+    }
+
+    /**
+     * @param pet the pet to set
+     */
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+    
+}
