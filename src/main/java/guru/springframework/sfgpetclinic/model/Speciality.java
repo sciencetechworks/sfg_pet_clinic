7c84b34964ae761e719f8e199b229e526c7891fa
ScienceTechWorks <sciencetechworks@gmail.com>
2021-11-15 16:37:48 +0100

    Create Vet Speciality Entity, Associate to Vet. Closes #21

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Speciality extends BaseEntity {
+    
+    private String description;
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -4,10 +4,29 @@
  */
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  *
  * @author Usuario
  */
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    /**
+     * @return the specialities
+     */
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    /**
+     * @param specialities the specialities to set
+     */
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+    
+    
     
 }
