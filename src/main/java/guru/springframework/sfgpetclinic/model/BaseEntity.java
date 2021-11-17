3ef3628c2ad492dc2556af15767d068937e616ca
ScienceTechWorks <sciencetechworks@gmail.com>
2021-11-17 18:08:38 +0100

    MappedSuperclass Entity. Closes #29

--- src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -5,13 +5,20 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 /**
  *
  * @author Usuario
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
     
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     /**
